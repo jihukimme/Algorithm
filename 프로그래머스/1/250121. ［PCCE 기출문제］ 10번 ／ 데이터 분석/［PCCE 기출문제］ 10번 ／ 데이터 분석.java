70c78ee73d7d8c1f96945e8032d9b5a3f67945ec
Jihu Kim <jihu0210@naver.com>
2025-01-23 14:56:53 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 2.82 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    static int extIdx = 0;
+    static int sortIdx = 0;
+    
+    public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
+        ArrayList<int[]> list = new ArrayList<>();
+        
+        String[] colName = {"code", "date", "maximum", "remain"};
+        
+        for(int i=0; i<colName.length; i++){
+            if(colName[i].equals(ext)){
+                extIdx = i;
+            }
+            if(colName[i].equals(sort_by)){
+                sortIdx = i;
+            }
+        }
+        
+        
+        for(int i=0; i<data.length; i++){
+            if(data[i][extIdx]<val_ext){
+                list.add(data[i]);
+            }
+        }
+        
+        list.sort((o1, o2) -> Integer.compare(o1[sortIdx], o2[sortIdx]));
+        
+        int[][] answer = new int[list.size()][4];
+        for(int i=0; i<list.size(); i++){
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+    }
+}
