30d09bb2bbb431388d06bb89d4c114a1c1670b3c
Jihu Kim <jihu0210@naver.com>
2025-01-25 13:30:15 +0900

    [level 2] Title: 소수 찾기, Time: 105.18 ms, Memory: 93.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+class Solution {
+    
+    HashSet<Integer> set = new HashSet<>();
+    boolean[] visited = new boolean[7];
+    
+    public int solution(String numbers) {
+        
+        int answer = 0;
+        
+        dfs(0, numbers, "");
+        
+        for(int n : set){
+            if(isPrime(n)){
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+    
+    
+    public void dfs(int depth, String numbers, String s){
+        if(depth > numbers.length()){
+            return;
+        }
+        
+        for(int i=0; i<numbers.length(); i++){
+            if(!visited[i]){
+                visited[i] = true;
+                set.add(Integer.parseInt(s+numbers.charAt(i)));
+                dfs(depth+1, numbers, s+numbers.charAt(i));
+                visited[i]=false;
+            }
+
+        }
+    }
+    
+    public boolean isPrime(int number){
+        if(number < 2)
+            return false;
+        
+        for(int i=2; i<number; i++){
+            if(number%i==0){
+                return false;
+            }
+        }
+       
+        return true;
+    }
+    
+}
