52fad3695ed6b1f51e4c415cbb6b9b166743b911
Jihu Kim <jihu0210@naver.com>
2025-01-26 15:26:06 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 1.27 ms, Memory: 87.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String answer = "";
+        
+        String[] arr = my_string.split("");
+        
+        String tmp = "";
+        tmp = arr[num1];
+        arr[num1] = arr[num2];
+        arr[num2] = tmp;
+        
+        for(int i=0; i<arr.length; i++){
+            answer += arr[i];
+        }
+        
+        return answer;
+    }
+}
