d7c6b914848276423f9cd811f3f484f079b145cc
Jihu Kim <jihu0210@naver.com>
2025-02-01 22:31:13 +0900

    [level 0] Title: 369게임, Time: 0.03 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.java
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -2,12 +2,20 @@ class Solution {
     public int solution(int order) {
         int answer = 0;
         
-        String[] arr = Integer.toString(order).split("");
+//         String[] arr = Integer.toString(order).split("");
         
-        for(int i=0; i<arr.length; i++){
-            if(arr[i].equals("3") || arr[i].equals("6") || arr[i].equals("9")){
+//         for(int i=0; i<arr.length; i++){
+//             if(arr[i].equals("3") || arr[i].equals("6") || arr[i].equals("9")){
+//                 answer++;
+//             }
+//         }
+        
+        while(order>0){
+            int num = order%10;
+            if(num == 3 || num == 6 || num == 9){
                 answer++;
             }
+            order /= 10;
         }
         
         return answer;
