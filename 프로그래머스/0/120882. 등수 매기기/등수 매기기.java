b0f79edffc5dbdad187e19cb296c9ac0bdf97d11
Jihu Kim <jihu0210@naver.com>
2025-02-02 14:44:35 +0900

    [level 0] Title: 등수 매기기, Time: 0.84 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.java
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.java
@@ -2,25 +2,31 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[][] score) {
-        int[] answer = new int[score.length];
+        int n = score.length;
         
-        Double[] arr = new Double[score.length]; 
+        int[] answer = new int[n];
+        Double[] arr = new Double[n];
         
-        for(int i=0; i<score.length; i++){
+        for(int i=0; i<n; i++){
             arr[i] = ((double)score[i][0] + (double)score[i][1])/2;
         }
         
         Arrays.sort(arr, Collections.reverseOrder());
         
-        for(int i=0; i<score.length; i++){
-            double average = ((double)score[i][0] + (double)score[i][1])/2;
-            
-            for(int j=0; j<score.length; j++){
-                if(average == arr[j]){
-                    answer[i] = j + 1;
-                    break;
-                }
+        HashMap<Double, Integer> map = new HashMap<>();
+        
+        int rank = 1;
+        for(int i=0; i<n; i++){
+            if(!map.containsKey(arr[i])){
+                map.put(arr[i], rank);
             }
+            rank++;
+        }
+        
+        for(int i=0; i<n; i++){
+            double avg = ((double)score[i][0] + (double)score[i][1])/2;
+            
+            answer[i] = map.get(avg);
         }
         
         return answer;
