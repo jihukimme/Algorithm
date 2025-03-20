26f23a7e71768495b930b028c44610d7a6e866cb
Jihu Kim <jihu0210@naver.com>
2025-03-20 14:04:51 +0900

    [Bronze II] Title: 상수, Time: 68 ms, Memory: 11516 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+
+        String[] arr = s.split(" ");
+
+        int num1 = Integer.parseInt(new StringBuilder().append(arr[0]).reverse().toString());
+        int num2 = Integer.parseInt(new StringBuilder().append(arr[1]).reverse().toString());
+        
+        System.out.println(num1>num2 ? num1 : num2);
+    }
+}
