27f90541b090fc4f3d15eedd367c3535e4ee56c9
Jihu Kim <jihu0210@naver.com>
2025-03-20 13:52:05 +0900

    [Bronze II] Title: 단어의 개수, Time: 276 ms, Memory: 51604 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+
+        String[] arr = s.split(" ");
+
+        int count = 0;
+
+        for(int i=0; i<arr.length; i++){
+            if(arr[i].isEmpty()){
+                continue;
+            }
+            count++;
+        }
+
+        System.out.println(count);
+    }
+}
