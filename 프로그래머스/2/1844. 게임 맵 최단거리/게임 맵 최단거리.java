4623fbe95665239bd75e036c9cafd1bf46d90ec6
Jihu Kim <jihu0210@naver.com>
2025-05-19 18:02:06 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.32 ms, Memory: 58.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -54,28 +54,67 @@ class Solution {
         }
         
         return -1;
-    }
+    }    
+}
+
+
+
+// import java.util.*;
+
+// class Solution {
+    
+//     boolean[][] isVisited;
+    
+//     int[] dx = {0, 0, -1, 1};
+//     int[] dy = {1, -1, 0, 0};
+    
+//     ArrayList<Integer> list = new ArrayList<>();
     
     
-//     public void dfs(int x, int y, int[][] maps, int distance){
-//         if(x==maps.length-1 && y==maps[0].length-1){
-//             distance++;
+//     public int solution(int[][] maps) {
+//         int n = maps.length;
+//         int m = maps[0].length;
+        
+//         isVisited = new boolean[n][m];
+        
+//         isVisited[0][0] = true; // 첫 시작위치
+//         dfs(0, maps, 0, 0, 1);
+        
+//         Collections.sort(list);
+        
+//         if(list.isEmpty()){
+//             return -1;
+//         }
+        
+//         return list.get(0);
+//     }
+    
+//     void dfs(int depth, int[][] maps, int x, int y, int distance){ 
+//         if(depth == maps.length*maps[0].length){
+//             return;
+//         }
+        
+//         // if(x<0 || x>maps[0].length-1 || y<0 || y>maps.length-1){
+//         //     return;
+//         // }
+
+//         if(x==maps[0].length-1 && y==maps.length-1){
 //             list.add(distance);
 //             return;
 //         }
+        
         
 //         for(int i=0; i<4; i++){
 //             int nextX = x + dx[i];
 //             int nextY = y + dy[i];
             
-//             if(nextX>=0 && nextX<maps.length && nextY>=0 && nextY<maps[0].length){
-//                 if(maps[nextX][nextY] != 0 && !visited[nextX][nextY]){
-//                     visited[nextX][nextY] = true;
-//                     dfs(nextX, nextY, maps, distance+1);
-//                     visited[nextX][nextY] = false;
+//             if(nextX>=0 && nextX<=maps[0].length-1 && nextY>=0 && nextY<=maps.length-1){
+//                 if(maps[nextX][nextY]!=0 && !isVisited[nextX][nextY]){
+//                     isVisited[nextX][nextY] = true;
+//                     dfs(depth+1, maps, nextX, nextY, distance+1);
+//                     isVisited[nextX][nextY] = false;
 //                 }
 //             }
 //         }
 //     }
-    
-}
+// }
