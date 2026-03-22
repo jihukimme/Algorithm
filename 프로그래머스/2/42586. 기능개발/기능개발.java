6d152fef88907d42e3862dfb3f0dd7200143caea
Jihu Kim <jihu0210@naver.com>
2026-03-22 12:49:09 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,41 +2,82 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int[] answer;
         
-        ArrayList<Integer> list = new ArrayList<>();
         int[] days = new int[progresses.length];
+        int progress = 0;
         
-        
-        for(int i=0; i<progresses.length; i++){
-            days[i] = (100 - progresses[i]) / speeds[i];
-            if((100 - progresses[i]) % speeds[i]>0){
+        for(int i=0; i<days.length; i++){
+            days[i] = (100-progresses[i]) / speeds[i];
+            
+            if((100-progresses[i]) % speeds[i] > 0){
                 days[i]++;
             }
         }
-        
-        int x = days[0];
+                
+        ArrayList<Integer> list = new ArrayList<>();
         int count = 1;
         
-        for(int i=1; i<progresses.length; i++){
-            if(x>=days[i]){
+        // 7, 3, 9
+        
+        for(int i=1; i<days.length; i++){
+            
+            if(days[i-1]>=days[i]){
                 count++;
+                days[i] = days[i-1];
             }
             else{
                 list.add(count);
-                x = days[i];
                 count = 1;
             }
         }
         
         list.add(count);
         
-        answer = new int[list.size()];
         
-        for(int i=0; i<list.size(); i++){
+        int[] answer = new int[list.size()];
+        
+        for(int i=0; i<answer.length; i++){
             answer[i] = list.get(i);
         }
         
         return answer;
+        
+        
+//         int[] answer;
+        
+//         ArrayList<Integer> list = new ArrayList<>();
+//         int[] days = new int[progresses.length];
+        
+        
+//         for(int i=0; i<progresses.length; i++){
+//             days[i] = (100 - progresses[i]) / speeds[i];
+//             if((100 - progresses[i]) % speeds[i]>0){
+//                 days[i]++;
+//             }
+//         }
+        
+//         int x = days[0];
+//         int count = 1;
+        
+//         for(int i=1; i<progresses.length; i++){
+//             if(x>=days[i]){
+//                 count++;
+//             }
+//             else{
+//                 list.add(count);
+//                 x = days[i];
+//                 count = 1;
+//             }
+//         }
+        
+//         list.add(count);
+        
+//         answer = new int[list.size()];
+        
+//         for(int i=0; i<list.size(); i++){
+//             answer[i] = list.get(i);
+//         }
+        
+//         return answer;
     }
 }
