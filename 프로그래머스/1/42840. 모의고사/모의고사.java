3d0917253fe4531f6d64ac57891398fdf84041d4
Jihu Kim <jihu0210@naver.com>
2026-03-22 17:26:42 +0900

    [level 1] Title: 모의고사, Time: 0.90 ms, Memory: 89.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -3,34 +3,86 @@ import java.util.*;
 class Solution {
     public ArrayList<Integer> solution(int[] answers) {
         
-        int[] p1 = {1,2,3,4,5};
-        int[] p2 = {2,1,2,3,2,4,2,5};
-        int[] p3 = {3,3,1,1,2,2,4,4,5,5};
+        int[] a1 = {1, 2, 3, 4, 5}; // 5
+        int[] a2 = {2, 1, 2, 3, 2, 4, 2, 5}; // 8
+        int[] a3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}; // 10
         
-        int[] result = new int[3];
+        int[] score = new int[3];
         
         for(int i=0; i<answers.length; i++){
-            if(answers[i]==p1[i%5]){
-                result[0]++;
-            }
-            if(answers[i]==p2[i%8]){
-                result[1]++;
-            }
-            if(answers[i]==p3[i%10]){
-                result[2]++;
-            }
-        }
+            if(answers[i]==a1[i%5]){
+                    score[0]++;
+                }
+            
+            if(answers[i]==a2[i%8]){
+                    score[1]++;
+                }
         
-        int max = Math.max(result[0], Math.max(result[1], result[2]));
+            if(answers[i]==a3[i%10]){
+                    score[2]++;
+                }
+
+        }
         
         ArrayList<Integer> list = new ArrayList<>();
         
-        for(int i=0; i<result.length; i++){
-            if(result[i]==max){
+        int max = 0;
+        
+        for(int i=0; i<score.length; i++) {
+            if(max<score[i]){
+                max=score[i];
+            }
+        }
+        
+        for(int i=0; i<score.length; i++) {
+            if(max==score[i]){
                 list.add(i+1);
             }
         }
         
-        return list; 
+        
+        return list;
+        
+//         int[] p1 = {1,2,3,4,5};
+//         int[] p2 = {2,1,2,3,2,4,2,5};
+//         int[] p3 = {3,3,1,1,2,2,4,4,5,5};
+        
+//         int[] scores = new int[3];
+        
+//         for(int i=0; i<answers.length; i++){
+//             if(answers[i] == p1[i%5]){
+//                 scores[0]++;
+//             }
+            
+//             if(answers[i] == p2[i%8]){
+//                 scores[1]++;
+//             }
+            
+//             if(answers[i] == p3[i%10]){
+//                 scores[2]++;
+//             }
+//         }
+                
+//         int bestScore = scores[0];
+//         for(int i=1; i<scores.length; i++){
+//             if(bestScore < scores[i]){
+//                 bestScore = scores[i];
+//             }
+//         }
+        
+//         ArrayList<Integer> list = new ArrayList<>();
+        
+//         for(int i=0; i<scores.length; i++){
+//             if(bestScore == scores[i]){
+//                 list.add(i+1);
+//             }
+//         }
+        
+//         Collections.sort(list, Collections.reverseOrder());
+        
+//         return list;
+       
     }
 }
+
+
