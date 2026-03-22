fb0c60bb20ccd9e0e4a6271093a60634e0412f9e
Jihu Kim <jihu0210@naver.com>
2026-03-22 22:08:04 +0900

    [level 2] Title: 카펫, Time: 8.07 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,28 +1,44 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
-        
-        int size = brown + yellow;
         
         int row = 0;
         int col = 0;
         
-        // size == row*col;
-                
+        int size = brown + yellow;
         
         while(size != row*col){
             row++;
-            for(col = 1; col<row; col++){
-                if(size == row*col && yellow == (row-2) * (col-2)){
+            for(col=1; col<row; col++){
+                if(row*col == size && (row-2)*(col-2) == yellow){
                     break;
                 }
-            }            
+            }
         }
         
+        int[] arr = {row, col};
         
-        answer[0] = row;
-        answer[1] = col;
+        return arr;
         
-        return answer;
+        
+//         int[] answer = new int[2];
+        
+//         int size = brown + yellow;
+        
+//         int row = 0; // 가로
+//         int col = 0; // 세로
+        
+//         while(row * col != size){
+//             row++;
+//             for(col=1; col<row; col++){
+//                 if(col*row==size && yellow==(col-2)*(row-2)){
+//                     break;
+//                 }
+//             }
+//         }
+        
+//         answer[0] = row;
+//         answer[1] = col;
+        
+//         return answer;
     }
 }
