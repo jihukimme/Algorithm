9cb135753b84c1a5809061c704f34658b31e2fce
Jihu Kim <jihu0210@naver.com>
2026-03-23 23:28:17 +0900

    [Bronze I] Title: 세로읽기, Time: 68 ms, Memory: 11504 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -1,37 +1,32 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        // 5개의 문자열을 받음
-        // 5개의 문자열에서 같은 인덱스의 문자를 합침
-
         String[] arr = new String[5];
-        int maxLength = 0;
+
+        int max = 0;
 
         for(int i=0; i<5; i++){
             arr[i] = br.readLine();
-
-            if(maxLength < arr[i].length()){
-                maxLength = arr[i].length();
+            if(max<arr[i].length()){
+                max = arr[i].length();
             }
         }
 
-        StringBuilder sb = new StringBuilder();
+        String answer ="";
 
-        for(int i=0; i<maxLength; i++){
+        for(int i=0; i<max; i++){
             for(int j=0; j<5; j++){
-                if(i>=arr[j].length()) {
+                if(arr[j].length()<=i){
                     continue;
                 }
-                sb.append(arr[j].charAt(i));
+                answer+=arr[j].charAt(i);
             }
         }
 
-        System.out.println(sb);
-
+        System.out.println(answer);
     }
 }
