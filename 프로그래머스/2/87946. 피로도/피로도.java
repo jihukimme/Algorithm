c2f2f7a35f25bc2eecbbd624a037cc9aee457580
Jihu Kim <jihu0210@naver.com>
2026-03-23 12:06:29 +0900

    [level 2] Title: 피로도, Time: 15.98 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,42 +1,3 @@
-// import java.util.*;
-
-// class Solution {
-    
-//     ArrayList<Integer> countList;
-//     boolean[] visited;
-    
-//     public int solution(int k, int[][] dungeons) {
-        
-//         countList = new ArrayList<>();
-//         visited = new boolean[dungeons.length];
-        
-//         dfs(0, visited, k, dungeons, 0);
-        
-//         countList.sort(Collections.reverseOrder());
-        
-//         int maxCount = countList.get(0);
-        
-//         return maxCount;
-//     }
-    
-//     public void dfs(int depth, boolean[] visited, int k, int[][] dungeons, int count){
-//         if(depth>=dungeons.length){
-//             countList.add(count);
-//             return;
-//         }
-        
-//         for(int i=0; i<dungeons.length; i++){
-//             if(!visited[i] && dungeons[i][0]<=k){
-//                 visited[i] = true;
-            
-//                 dfs(depth+1, visited, k-dungeons[i][1], dungeons, count+1);
-//                 visited[i] = false;
-//             }
-//         }
-//     }
-    
-// }
-
 import java.util.*;
 
 class Solution {
@@ -47,35 +8,69 @@ class Solution {
     public int solution(int k, int[][] dungeons) {
         isVisited = new boolean[dungeons.length];
         
-        dfs(0, k, dungeons, 0);
+        dfs(0, k, dungeons);
         
-        int max = 0;
-        for(int n : list){
-            if(max<n) {
-                max = n;
-            }
-        }
-       
-        return max;
-    }
+        Collections.sort(list, Collections.reverseOrder());
+        
+        return list.get(0);
+    }    
     
-    void dfs(int depth, int k, int[][] dungeons, int count){
-        if(depth>dungeons.length) {
+    public void dfs(int depth, int k, int[][] dungeons){
+        if(depth>dungeons.length){
             return;
         }
         
         for(int i=0; i<dungeons.length; i++){
-            if(!isVisited[i] && dungeons[i][0] <= k){
+            if(!isVisited[i] && k>=dungeons[i][0]){
                 isVisited[i] = true;
-                if(k-dungeons[i][1] >= 0){
-                    // k-=dungeons[i][1];
-                    list.add(count+1);
-                    dfs(depth+1, k-dungeons[i][1], dungeons, count+1);
-                }
+                list.add(depth+1);
+                dfs(depth+1, k-dungeons[i][1], dungeons);
                 isVisited[i] = false;
             }
         }
     }
     
-    
 }
+
+// import java.util.*;
+
+// class Solution {
+    
+//     ArrayList<Integer> list = new ArrayList<>();
+//     boolean[] isVisited;
+    
+//     public int solution(int k, int[][] dungeons) {
+//         isVisited = new boolean[dungeons.length];
+        
+//         dfs(0, k, dungeons, 0);
+        
+//         int max = 0;
+//         for(int n : list){
+//             if(max<n) {
+//                 max = n;
+//             }
+//         }
+       
+//         return max;
+//     }
+    
+//     void dfs(int depth, int k, int[][] dungeons, int count){
+//         if(depth>=dungeons.length) {
+//             return;
+//         }
+        
+//         for(int i=0; i<dungeons.length; i++){
+//             if(!isVisited[i] && dungeons[i][0] <= k){
+//                 isVisited[i] = true;
+//                 if(k-dungeons[i][1] >= 0){
+//                     // k-=dungeons[i][1];
+//                     list.add(count+1);
+//                     dfs(depth+1, k-dungeons[i][1], dungeons, count+1);
+//                 }
+//                 isVisited[i] = false;
+//             }
+//         }
+//     }
+    
+    
+// }
