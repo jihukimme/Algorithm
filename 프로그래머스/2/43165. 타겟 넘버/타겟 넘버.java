823f12b39674c2570a1aedf00bcc04bfde8b65a6
Jihu Kim <jihu0210@naver.com>
2026-03-23 14:42:19 +0900

    [level 2] Title: 타겟 넘버, Time: 7.34 ms, Memory: 93.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,23 +1,48 @@
+// class Solution {
+    
+//     int count = 0;
+    
+//     public int solution(int[] numbers, int target) {
+//         dfs(0, numbers, target, 0);
+        
+//         return count;
+//     }
+    
+//     public void dfs(int depth, int[] numbers, int target, int result){
+//         if(depth==numbers.length){
+//             if(target==result){
+//                 count++;
+//             }
+//             return;
+//         }
+        
+//         dfs(depth+1, numbers, target, result+numbers[depth]);
+//         dfs(depth+1, numbers, target, result-numbers[depth]);
+//     }
+// }
+
+
+
 class Solution {
     
     int count = 0;
     
     public int solution(int[] numbers, int target) {
-        
         dfs(0, numbers, target, 0);
+        
         return count;
     }
     
     public void dfs(int depth, int[] numbers, int target, int result){
-        if(depth >= numbers.length){
-            if(target == result){
+        if(depth==numbers.length){
+            if(target==result){
                 count++;
             }
-            
             return;
         }
         
         dfs(depth+1, numbers, target, result+numbers[depth]);
         dfs(depth+1, numbers, target, result-numbers[depth]);
     }
+
 }
